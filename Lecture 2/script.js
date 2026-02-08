68742074be3e0a99161a9f3781fd9ebb2eff691a
Faruque5698 <ashaduzzaman5698@gmail.com>
2026-02-08 22:35:46 +0600

    conditional and opertior, unary, assignment

---
+++ Lecture 2/script.js
@@ -0,0 +1,64 @@
+// // // console.log("Hello World");
+
+// // let a = 5;
+// // let b = 10;
+// // // console.log(a + b);
+// // console.log("a + b =", a + b);
+// // console.log("a - b =", a - b);
+// // console.log("a * b =", a * b);
+// // console.log("a / b =", a / b);
+// // console.log("a % b =", a % b); //modulus operator
+// // console.log("a ** b =", a ** b); //exponentiation operator
+
+// // // Increment and Decrement Operators Unary operators
+// // let c = 5;
+// // console.log("c =", c);
+// // console.log("c++ =", c++);
+// // console.log("c =", c);
+// // console.log("++c =", ++c);
+// // console.log("c =", c);
+// // console.log("--c =", --c);
+// // console.log("c =", c);
+// // console.log("c-- =", c--);
+// // console.log("c =", c);
+
+// // //assignment operators
+// // let d = 5;
+// // console.log("d =", d);
+// // d += 5; //d = d + 5
+// // console.log("d += 5 =", d);
+// // d -= 2; //d = d - 2
+// // console.log("d -= 2 =", d);
+// // d *= 3; //d = d * 3
+// // console.log("d *= 3 =", d);
+// // d /= 4; //d = d / 4
+// // console.log("d /= 4 =", d);
+// // d %= 3; //d = d % 3
+// // console.log("d %= 3 =", d);
+// // d **= 2; //d = d ** 2
+// // console.log("d **= 2 =", d);
+
+// //  comparison operators
+// let e = 5;
+// let f = 10;
+// console.log("e == f =", e == f); //equality operator (loose equality)
+// console.log("e === f =", e === f); //strict equality operator
+// console.log("e != f =", e != f); //inequality operator (loose inequality)
+// console.log("e !== f =", e !== f); //strict inequality operator
+// console.log("e > f =", e > f); //greater than operator
+// console.log("e < f =", e < f); //less than operator
+// console.log("e >= f =", e >= f); //greater than or equal to operator
+// console.log("e <= f =", e <= f); //less than or equal to operator
+
+//conditional Statement
+let a = 5;
+let b = 7;
+let d = 10;
+
+if (a > b && a > d) {
+  console.log("a is greater than b and d");
+} else if (a < b && d < b) {
+    console.log ("b is greater than a and d");
+} else {
+  console.log("d is greater than a and b");
+} 
